34c09faba8e93ef023598c9ed17bc8f84f2ff2f2
Lewis MacNee <lewis.macnee@hotmail.com>
2016-11-21 19:48:30 +0000

    added lizard and spock

--- controller.rb
+++ controller.rb
@@ -4,8 +4,8 @@ require('json')
 require_relative('models/rock_paper_scissors')
 require('pry-byebug')
 
-get '/'do
-return "sup"
+get '/RPS/info' do
+  erb(:information)
 end
 
 get '/RPS/:choice1' do
@@ -18,6 +18,10 @@ get '/RPS/:choice1' do
     result = user_choice.gameplay_paper()
   elsif input1 == "scissors"
     result = user_choice.gameplay_scissors()
+  elsif input1 == "lizard"
+    result = user_choice.gameplay_lizard()
+  elsif input1 == "spock"
+    result = user_choice.gameplay_spock()
   end
   @rps_result = result.to_s
   erb(:result)
@@ -26,6 +30,7 @@ end
 get '/RPS/:choice1/:choice2' do
   input1 = params[:choice1]
   input2 = params[:choice2]
+# binding.pry
   options = {player_2_choice: input2}
   user_choice = RockPaperScissors.new(options)
   if input1 == "rock"
@@ -34,8 +39,15 @@ get '/RPS/:choice1/:choice2' do
     result = user_choice.gameplay_paper()
   elsif input1 == "scissors"
     result = user_choice.gameplay_scissors()
+  elsif input1 == "lizard"
+    result = user_choice.gameplay_lizard()
+  elsif input1 == "spock"
+    result = user_choice.gameplay_spock()
   end
   @rps_result = result.to_s
   erb(:result)
-  
+end
+
+get '/RPS/egg'do
+return "Yay, you've found the easter egg, now go play the game properly"
 end
--- models/rock_paper_scissors.rb
+++ models/rock_paper_scissors.rb
@@ -1,18 +1,22 @@
 class RockPaperScissors
 
   def initialize(options)
-    rps_array = ["rock", "paper", "scissors"]
-    @rand_choice = rps_array[rand(3)]
-    @player2 = options[:player2_choice] || @rand_choice
+    rps_array = ["rock", "paper", "scissors", "lizard", "spock"]
+    @rand_choice = rps_array[rand(5)]
+    @player2 = options[:player_2_choice] || @rand_choice
   end
 
   def gameplay_rock()
     if @player2 == "rock"
       result = "Rock on Rock: draw"
     elsif @player2 == "paper"
-      result = "Paper beats Rock: you lose"
+      result = "Paper covers Rock: you lose"
+    elsif @player2 == "spock"
+      result = "Spock vaporizes Rock: you lose"
     elsif @player2 == "scissors"
-      result = "Rock beats Scissors: you win"
+      result = "Rock crushes Scissors: you win"
+    elsif @player2 == "lizard"
+      result = "Rock crushes Lizard: you win"
     end
     return result
   end
@@ -20,10 +24,14 @@ class RockPaperScissors
   def gameplay_paper()
     if @player2 == "paper"
       result = "Paper on Paper: draw"
-    elsif @player2 == "rock"
-      result = "Paper beats Rock: you win"
     elsif @player2 == "scissors"
-      result = "Scissors beats Paper: you lose"
+      result = "Scissors cuts Paper: you lose"
+    elsif @player2 == "lizard"
+      result = "Lizard eats Paper: you lose"
+    elsif @player2 == "rock"
+      result = "Paper covers Rock: you win"
+    elsif @player2 == "spock"
+      result = "Paper disproves Spock: you win"
     end
     return result
   end
@@ -31,10 +39,44 @@ class RockPaperScissors
   def gameplay_scissors()
     if @player2 == "scissors"
       result = "Scissors on Scissors: draw"
+    elsif @player2 == "rock"
+      result = "Rock crushes Scissors: you lose"
+    elsif @player2 == "spock"
+      result = "Spock smashes Scissors: you lose"
     elsif @player2 == "paper"
-      result = "Scissors beats Paper: you win"
+      result = "Scissors cuts paper: you win"
+    elsif @player2 == "lizard"
+      result = "Scissors decapitates Lizard: you win"
+    end
+    return result
+  end
+
+  def gameplay_lizard()
+    if @player2 == "lizard"
+      result = "Lizard on Lizard: draw"
+    elsif @player2 == "rock"
+      result = "Rock crushes Lizard: you lose"
     elsif @player2 == "scissors"
-      result = "Rock beats Scissors: you lose"
+      result = "Scissors decapitates Lizard: you lose"
+    elsif @player2 == "paper"
+      result = "Lizard eats Paper: you win"
+    elsif @player2 == "spock"
+      result = "Lizard poisons Spock: you win"
+    end
+    return result
+  end
+
+  def gameplay_spock()
+    if @player2 == "spock"
+      result = "Spock on Spock: draw"
+    elsif @player2 == "paper"
+      result = "Paper disproves Spock: you lose"
+    elsif @player2 == "lizard"
+      result = "Lizard poisons Spock: you lose"
+    elsif @player2 == "scissors"
+      result = "Spock smashes Scissors: you win"
+    elsif @player2 == "rock"
+      result = "Spock vaporizes Rock: you win"
     end
     return result
   end
